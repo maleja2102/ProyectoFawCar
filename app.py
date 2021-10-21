7520b2851da9e41bc927d22e83f3f22b623d6cc5
jespitiaau <91152504+jespitiaau@users.noreply.github.com>
2021-10-20 21:08:59 -0500

    SQLAlchemy

--- app.py
+++ app.py
@@ -1,16 +1,66 @@
-from flask import Flask, render_template, jsonify, redirect, request, sessions
+from enum import unique
+from flask import Flask, render_template, jsonify, redirect, request,session
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import format_string
+from werkzeug.security import *
+from werkzeug.utils import redirect
 import os
 
-from listas import usuarios1, productos1, proveedores1
+from wtforms.fields.simple import PasswordField
+from wtforms.validators import Email
 from login import formIngreso
+from register import formRegistro
+
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+db=SQLAlchemy(app)
 rol_usuario=""
 
-lista_usuarios = ["usuario", "admin", "superadmin"]
-lista_inventario = ["mazda 3", "chevrolet onix", "mazda ñao"]
-lista_proveedores = ["Kia", "General Motors", "Volkswagen"]
+
+#modelado base de datos
+class core(db.Model):
+    __abstract__ = True
+    id = db.Column(db.Integer, primary_key = True)
+
+class Users(core):
+    __tableName__ = "Users"
+    username= db.Column(db.String, unique=True, nullable=False)
+    password= db.Column(db.String, nullable=False)
+    role= db.Column(db.String, nullable=False)
+    email= db.Column(db.String, unique=True, nullable=False)
+    name= db.Column(db.String)
+    surname= db.Column(db.String)
+    personalId=db.Column(db.String, unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Users %r>' % self.username
+
+
+class Cars(core):
+    __tableName__ = "Cars"
+    brand= db.Column(db.String, nullable=False)
+    model= db.Column(db.String, nullable=False)
+    description= db.Column(db.String)
+    year= db.Column(db.String,  nullable=False)
+    stock= db.Column(db.String)
+    type= db.Column(db.String)
+
+    def __repr__(self):
+        return '<Cars %r>' % self.description
+
+class Supplier(core):
+    __tableName__ = "Suppliers"
+    name= db.Column(db.String, unique=True, nullable=False)
+    description= db.Column(db.String)
+    contact= db.Column(db.String, nullable=False)
+    phone= db.Column(db.String)
+    email= db.Column(db.String,unique=True, nullable=False)
+    address= db.Column(db.String)
+    TributaryId=db.Column(db.String, unique=True, nullable=False)
+    def __repr__(self):
+        return '<Suppliers %r>' % self.name
 
 
 # Rutas principales
@@ -26,28 +76,20 @@ def iniciar():
     if request.method=='POST':
         #Si la solicitud es POST
         #Traer usuario y contraseña del formulario
-            # user=request.form.get('user')
-            # passw=request.form.get('pass')
             formu=formIngreso()
             user = formu.usuario.data
             password = formu.contrasena.data
             #Recorremos la lista en busca del usuario y la contraseña introducidos
-            login = [usuario for usuario in usuarios1 if (usuario["nombre"] == user)and (usuario["contrasena"]==password)]
-            #Si la lista resultante está vacia, enviamos error
-            # rol_usuario= login[0]["rol"]
-            global rol_usuario 
-            rol_usuario= login[0]["rol"]
-            if (len(login) == 0):
-                return jsonify({"mensaje": "usuario no encontrado"})
+            login=Users.query.filter_by(username=user).first()
+            if login:
+                if login.password == password:
+                    session["user"]=login.username
+                    session["rol"]=login.role
+                    return redirect("inicio")
             else:
-                #si la lista resultante contiene un elemento (siempre debe ser 1)
-                #extraemos el rol del usuario de la lista
-                # rol_usuario= login[0]["rol"]
-                #mostramos la plantilla de inicio, pasandole como parametro el rol del usuario
-                return render_template('inicio.html',rol_usuario= rol_usuario)
-                # q=request.form.get(rol_usuario)
+                return jsonify({"mensaje": "usuario no encontrado"})
     else:
-        return render_template('inicio.html',rol_usuario=rol_usuario)
+        return render_template('inicio.html')
 
 @app.route('/usuarios')
 def usuario():
@@ -71,93 +113,28 @@ def inventario():
 def prueba():
     return render_template('prueba.html')
 
-@app.route('/inventariosuperadmin')
-def inventariosuperadmin():
-    return render_template('inventario.html')
-
-@app.route('/prueba_proveedores')
-def prueba_proveedores():
-    return render_template('proveedores.html')
-
-#rol_usuario="admin"
-
-#ejemplo
 @app.route('/registro', methods=['GET','POST'])
-def regisro():
-    return "render_template('registro.html')"
-# Rutas para el superadmin
-@app.route('/inicio_superadmin', methods=['GET'])
-def inicio():
-    return "Inicia sesión"
 
-@app.route('/perfil_superadmin', methods=['GET','POST'])
-def perfil():
-    return "Bienvenido, aquí podrás cambiar la información de tu cuenta"
+def registro():
+    form=formRegistro()
+    if request.method=='POST':
+        username= form.username.data
+        password=form.password.data
+        confirm=form.confirm.data
+        email=form.email.data
+        name=form.name.data
+        surname=form.surname.data
+        personalId=form.personalId.data
 
-@app.route('/usuarios_superadmin/<id_usuario>', methods=['GET','POST'])
-def usuarios(id_usuario):
-    if id_usuario in lista_usuarios:
-        return f"Bienvenido, {id_usuario}"
-    else:
-        return f"El usuario {id_usuario} no está en la lista"
+        if password==confirm:
+            user=Users(username=username,password=password,email=email,name=name,surname=surname,personalId=personalId,role='registred')
+            db.session.add(user)
+            db.session.commit()
+            # return render_template('inicio.html')
+            
 
-@app.route('/inventario_superadmin/<id_carro>', methods=['GET','POST'])
-def inventario1(id_carro):
-    if id_carro in lista_inventario:
-        return f"Aún tienes el vehiculo, {id_carro}"
-    else:
-        return f"No tienes el vehículo {id_carro}"
-
-@app.route('/proveedores_superadmin/<id_proveedor>', methods=['GET','POST'])
-def proveedores(id_proveedor):
-    if id_proveedor in lista_proveedores:
-        return f"El proveedor {id_proveedor} aún está activo"
-    else:
-        return f"El proveedor {id_proveedor} ya no está activo"
-
-
-# Rutas para el admin
-@app.route('/administrador/<string:dato>', methods=['GET','POST'])
-def usuariosAdmin (dato):
-    usua = [usuario for usuario in usuarios1 if (usuario["rol"] == dato)]
-    if (len(usua) == 0):
-        return jsonify({"mensaje": "usuario no encontrado"})
-    else:
-        return jsonify({"mensaje": "articulo encontrado", "articulo":usua})
-
-@app.route('/proveedoradministrador/<string:info>', methods=['GET','POST'])
-def proveedoresAdmin(info):
-    provee = [proveedor for proveedor in proveedores1 if (proveedor["Contacto"] == info)]
-    if (len(provee) == 0):
-        return jsonify({"mensaje": "usuario no encontrado"})
-    else:
-        return jsonify({"mensaje": "articulo encontrado", "articulo":provee})
-
-@app.route('/administradorproductos/<string:informacion>', methods=['GET','POST'])
-def productosAdmin(informacion):
-    produc = [producto for producto in productos1 if (producto["Marca"] == informacion)]
-    if (len(produc) == 0):
-        return jsonify({"mensaje": "usuario no encontrado"})
-    else:
-        return jsonify({"mensaje": "articulo encontrado", "articulo":produc})
-
-
-# Rutas para el usuario final
-@app.route('/usuarioProveedores/<string:info>', methods=['GET','POST'])
-def proveedoresUsuario(info):
-    provee = [proveedor for proveedor in proveedores1 if (proveedor["Contacto"] == info)]
-    if (len(provee) == 0):
-        return jsonify({"mensaje": "usuario no encontrado"})
-    else:
-        return jsonify({"mensaje": "articulo encontrado", "articulo":provee})
-
-@app.route('/usuarioProductos/<string:informacion>', methods=['GET','POST'])
-def productosUsuario(informacion):
-    produc = [producto for producto in productos1 if (producto["Modelo"] == informacion)]
-    if (len(produc) == 0):
-        return jsonify({"mensaje": "usuario no encontrado"})
-    else:
-        return jsonify({"mensaje": "articulo encontrado", "articulo":produc})
+    return render_template('registro.html',form=form)
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(host='127.0.0.1', port=8000, debug=True)
