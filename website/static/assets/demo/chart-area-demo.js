0c920256e0715a1cb877fdae91bd58b7d55796e4
GiselleOrtizUpa <ogiselle@uninorte.edu.co>
2021-10-28 18:10:10 -0500

    Carga de datos ( proveedores- inventarios)
    Se cargaron 10 proveedores, y 50 productos.
    Adicional con la informacion generé los graficos del inicio

--- website/static/assets/demo/chart-area-demo.js
+++ website/static/assets/demo/chart-area-demo.js
@@ -7,9 +7,9 @@ var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
+    labels: ["2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022"],
     datasets: [{
-      label: "Sessions",
+      label: "Existencias",
       lineTension: 0.3,
       backgroundColor: "rgba(2,117,216,0.2)",
       borderColor: "rgba(2,117,216,1)",
@@ -20,7 +20,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBackgroundColor: "rgba(2,117,216,1)",
       pointHitRadius: 50,
       pointBorderWidth: 2,
-      data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
+      data: [28, 46, 54, 33, 55, 42, 30, 38],
     }],
   },
   options: {
@@ -33,14 +33,14 @@ var myLineChart = new Chart(ctx, {
           display: false
         },
         ticks: {
-          maxTicksLimit: 7
+          maxTicksLimit: 8
         }
       }],
       yAxes: [{
         ticks: {
           min: 0,
-          max: 40000,
-          maxTicksLimit: 5
+          max: 70,
+          maxTicksLimit: 10
         },
         gridLines: {
           color: "rgba(0, 0, 0, .125)",
