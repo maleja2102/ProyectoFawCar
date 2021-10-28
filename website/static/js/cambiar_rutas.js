d29bbd88399276d5da6bec4c4dfc62e2a5bbce3e
jespitiaau <91152504+jespitiaau@users.noreply.github.com>
2021-10-28 12:02:34 -0500

    Colores en el grid de invntario

--- website/static/js/cambiar_rutas.js
+++ website/static/js/cambiar_rutas.js
@@ -77,8 +77,22 @@ $(document).ready( function () {
         //         "visible": false,
         //     }
         // ]
-
+        'rowCallback': function(row, data, index){
+            if(data[3] < data[5]){
+                $(row).find('td:eq(3)').css('color', 'white');
+                $(row).find('td:eq(3)').addClass('bg-danger')
+            }
+            if(data[3] == data[5]){
+                $(row).find('td:eq(3)').css('color', 'white');
+                $(row).find('td:eq(3)').addClass('bg-warning')
+            }
+            if(data[3] > data[5]){
+                $(row).find('td:eq(3)').css('color', 'white');
+                $(row).find('td:eq(3)').addClass('bg-success')
+            }
+          }
     });
+
     
     $("#usertable tbody tr").on('click',function(event) {
         $("#usertable tbody tr").removeClass('bg-primary');
